80e17abac039ce2196f5cf8a3e4149ee930e5181
Asmahan355 <asmahan.elmhassani@etu.uae.ac.ma>
2024-10-01 01:17:05 +0200

    faceSnapComponent

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit{
+@Input() faceSnap!:FaceSnap;
+snapButtonText!:string;
+userHasSnapped!:boolean;
+ngOnInit(): void {
+  this.snapButtonText='Oh Snap!';
+  this.userHasSnapped=false;
+
+
+
+}
+//pour réagir avec un évenement venant du Dom
+//on commence son nom par : on...
+onSnap():void{
+  if(this.userHasSnapped){//true
+    this.unSnap();
+  }else{
+    this.snap();
+    
+  }
+  
+}
+unSnap(){
+    this.faceSnap.removeSnap();
+    this.snapButtonText='Oh Snap!';
+    this.userHasSnapped=false;
+}
+snap(){
+  this.faceSnap.addSnap();
+    this.snapButtonText='Oops, unSnap!';
+    this.userHasSnapped=true;
+}
+
+}
